39eca9d8ce4d7527a0a80b922f4063769fdeef91
natthawatthap <natthawatthap@gmail.com>
2024-03-02 02:07:45 +0700

    add auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,187 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRouteLoaderData, useFetcher } from "react-router-dom";
+import { Link, Outlet, RouterProvider, createBrowserRouter, redirect, useActionData, useLocation, useNavigation,Form } from "react-router-dom";
+import { fakeAuthProvider } from "./auth";
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    loader() {
+      // Our root route always provides the user, if logged in
+      return { user: fakeAuthProvider.username };
+    },
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: PublicPage,
+      },
+      {
+        path: "login",
+        action: loginAction,
+        loader: loginLoader,
+        Component: LoginPage,
+      },
+      {
+        path: "protected",
+        loader: protectedLoader,
+        Component: ProtectedPage,
+      },
+    ],
+  },
+  {
+    path: "/logout",
+    async action() {
+      // We signout in a "resource route" that we can hit from a fetcher.Form
+      await fakeAuthProvider.signout();
+      return redirect("/");
+    },
+  },
+]);
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
+  );
 }
 
-export default App
+function Layout() {
+  return (
+    <div>
+      <h1>Auth Example using RouterProvider</h1>
+
+      <p>
+        This example demonstrates a simple login flow with three pages: a public
+        page, a protected page, and a login page. In order to see the protected
+        page, you must first login. Pretty standard stuff.
+      </p>
+
+      <p>
+        First, visit the public page. Then, visit the protected page. You're not
+        yet logged in, so you are redirected to the login page. After you login,
+        you are redirected back to the protected page.
+      </p>
+
+      <AuthStatus />
+
+      <ul>
+        <li>
+          <Link to="/">Public Page</Link>
+        </li>
+        <li>
+          <Link to="/protected">Protected Page</Link>
+        </li>
+      </ul>
+
+      <Outlet />
+    </div>
+  );
+}
+
+function AuthStatus() {
+  // Get our logged in user, if they exist, from the root route loader data
+  let { user } = useRouteLoaderData("root") || { user: null };
+  let fetcher = useFetcher();
+
+  if (!user) {
+    return <p>You are not logged in.</p>;
+  }
+
+  let isLoggingOut = fetcher.formData != null;
+
+  return (
+    <div>
+      <p>Welcome {user}!</p>
+      <fetcher.Form method="post" action="/logout">
+        <button type="submit" disabled={isLoggingOut}>
+          {isLoggingOut ? "Signing out..." : "Sign out"}
+        </button>
+      </fetcher.Form>
+    </div>
+  );
+}
+
+async function loginAction({ request }) {
+  let formData = await request.formData();
+  let username = formData.get("username") || null;
+
+  // Validate our form inputs and return validation errors via useActionData()
+  if (!username) {
+    return {
+      error: "You must provide a username to log in",
+    };
+  }
+
+  // Sign in and redirect to the proper destination if successful.
+  try {
+    await fakeAuthProvider.signin(username);
+  } catch (error) {
+    // Unused as of now but this is how you would handle invalid
+    // username/password combinations - just like validating the inputs
+    // above
+    return {
+      error: "Invalid login attempt",
+    };
+  }
+
+  let redirectTo = formData.get("redirectTo") || "/";
+  return redirect(redirectTo);
+}
+
+async function loginLoader() {
+  if (fakeAuthProvider.isAuthenticated) {
+    return redirect("/");
+  }
+  return null;
+}
+
+function LoginPage() {
+  let location = useLocation();
+  let params = new URLSearchParams(location.search);
+  let from = params.get("from") || "/";
+
+  let navigation = useNavigation();
+  let isLoggingIn = navigation.formData?.get("username") != null;
+
+  let actionData = useActionData() || { error: undefined };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>You must log in to view the page at {from}</p>
+
+      <Form method="post" replace={true}>
+        <input type="hidden" name="redirectTo" value={from} />
+        <label>
+          Username: <input name="username" />
+        </label>{" "}
+        <button type="submit" disabled={isLoggingIn}>
+          {isLoggingIn ? "Logging in..." : "Login"}
+        </button>
+        {actionData.error ? (
+          <p style={{ color: "red" }}>{actionData.error}</p>
+        ) : null}
+      </Form>
+    </div>
+  );
+}
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function protectedLoader({ request }) {
+  // If the user is not logged in and tries to access `/protected`, we redirect
+  // them to `/login` with a `from` parameter that allows login to redirect back
+  // to this page upon successful authentication
+  if (!fakeAuthProvider.isAuthenticated) {
+    let params = new URLSearchParams();
+    params.set("from", new URL(request.url).pathname);
+    return redirect("/login?" + params.toString());
+  }
+  return null;
+}
+
+function ProtectedPage() {
+  return <h3>Protected</h3>;
+}
